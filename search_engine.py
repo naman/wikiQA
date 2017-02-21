71be8dea7dce12c5f62389d2b84810a2ca8d31a0
naman <namanspace@gmail.com>
2017-02-22 02:25:49 +0530

    precision+recall+cache

--- search_engine.py
+++ search_engine.py
@@ -8,9 +8,6 @@ import nltk
 import string
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
-
 
 def tab_split(string):
     return string.strip().split("\t")
@@ -49,6 +46,36 @@ def get_article_name(path, file_path):
         doc = words_in_line[5]
         if doc == file_path:
             return title
+
+
+def search_doc(set, article_name):
+    path = path_to_documents + set + "/question_answer_pairs.txt"
+    f = open(path)
+    for line in f:
+        words_in_line = tab_split(line)
+        title = ' '.join(clean_words(special_split(words_in_line[0])))
+        doc = words_in_line[5]
+        # s = cosine_sim(title, article_name)
+        # print s
+        # if s > 0.1:
+        if title == article_name:
+            return doc
+    return -1
+
+
+def get_doc_name(article_name):
+    a = search_doc("S08", article_name)
+
+    if isinstance(a, basestring):
+        return a
+    b = search_doc("S09", article_name)
+
+    if isinstance(b, basestring):
+        return b
+
+    c = search_doc("S10", article_name)
+    if isinstance(c, basestring):
+        return c
 
 
 def parse_set(set):
@@ -140,9 +167,10 @@ def cosine_sim(a, b):
     return magnitude[0, 1]
 
 
-def load_index_in_memory(var):
+def load_index_in_memory():
     with open(sys.argv[1]) as data_file:
         var = dict(json.load(data_file))
+    return var
 
 
 def clean_words(array):
@@ -156,54 +184,79 @@ def clean_words(array):
     return cleaned_words
 
 
-def jaccard_similarity(key, focus_terms):
-    word_set = set(focus_terms)
+def jaccard_similarity(article_name, focus_terms):
+
     scores = {}
-    all_lists = [focus_terms]
-    for x in file_sentences[key]:
+    for x in file_sentences[article_name]:
+        all_lists = [focus_terms]
+        word_set = set(focus_terms)
         words_in_sentence = clean_words(special_split(x))
         word_set.update(words_in_sentence)
         all_lists.append(words_in_sentence)
         intersect = intersection(all_lists)
+
         scores[x] = len(intersect)
-        all_lists.remove(words_in_sentence)
-
-    length = float(len(word_set))
-
-    # normalize
-    for x in scores:
+        length = float(len(word_set))
         scores[x] /= length
 
-    print_scores(scores, "Jaccard")
+    print_scores(article_name, scores, "Jaccard")
 
 
-def print_scores(scores, similarity):
-    print "\n", similarity, "similarity"
+def print_scores(retrieved_docs, retrieved_docs_count, article_name, scores, similarity):
+    if similarity == "Jaccard":
+        cuttoff = 0.004
+        threshold = 0.0001
+    elif similarity == "Cosine":
+        cuttoff = 0.1
+        threshold = 0.0001
+
     max_score = max(scores[x] for x in scores)
     for x in scores:
-        if math.fabs(max_score - scores[x]) < 0.0001:
-            print "[" + str(scores[x]) + "]", "\t", x
+        if scores[x] > cuttoff and math.fabs(max_score - scores[x]) < threshold:
+            retrieved_docs_count += 1
+            retrieved_docs[article_name] = True
+            print "\nDocument:", article_name, get_doc_name(article_name)
+            print "\t", similarity, "similarity"
+            print "\t[" + str(scores[x]) + "]", "\t", x
 
 
-def cosine_similarity(key, query):
+def cosine_similarity(retrieved_docs, retrieved_docs_count, article_name, query):
     scores = {}
-    for x in file_sentences[key]:
+    for x in file_sentences[article_name]:
         score = cosine_sim(query, x)
         scores[x] = score
-    print_scores(scores, "Cosine")
+    print_scores(retrieved_docs, retrieved_docs_count,
+                 article_name, scores, "Cosine")
 
 
 def process_query(query):
     focus_terms = clean_words(special_split(query))
-    key = ""
+    ground_truth(query)
+
+    relevant_docs = {doc: False for doc in file_sentences}
+    retrieved_docs = {doc: False for doc in file_sentences}
+
+    relevant_docs_count = 0
+    retrieved_docs_count = 0
     for word in focus_terms:
-        # search the article first
-        for x in file_sentences:
-            if word in x:
-                key = x
-                cosine_similarity(key, query)
-                jaccard_similarity(key, focus_terms)
-                ground_truth(query)
+        for doc in file_sentences:
+            if word in doc:
+                relevant_docs[doc] = True
+                relevant_docs_count += 1
+                break
+
+    for doc in file_sentences:
+        cosine_similarity(retrieved_docs, retrieved_docs_count, doc, query)
+        # jaccard_similarity(doc, focus_terms)
+
+    a = [doc for doc in relevant_docs if relevant_docs[doc] is True]
+    b = [doc for doc in relevant_docs if retrieved_docs[doc] is True]
+    intersect = intersection([a, b])
+    precision = len(intersect) / float(len(b))
+    recall = len(intersect) / float(len(a))
+
+    print "Precision:", precision
+    print "Recall:", recall
 
 
 def run_query(query):
@@ -226,10 +279,11 @@ if len(sys.argv) < 4:
 
     exit(1)
 
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
+
 path_to_documents = sys.argv[3]
 file_sentences = {}
-
-
 FOCUS_DISTANCE = 2
 stop_words = []
 delimiters = ['\n', ' ', ',', '.', '?', '!', ':', '#', '$', '[', ']',
@@ -245,8 +299,10 @@ def stem_tokens(tokens):
     return [porter.stem(item, 0, len(item) - 1) for item in tokens]
 
 
-def normalize(text):
-    return stem_tokens(nltk.word_tokenize(text.lower().translate(remove_punctuation_map)))
+def normalize(t):
+    x = stem_tokens(nltk.word_tokenize(
+        t.lower().translate(remove_punctuation_map)))
+    return x
 
 
 porter = PorterStemmer()
@@ -257,15 +313,33 @@ print ".........................................................."
 print "\t\tWelcome to WikiQA!"
 print "..........................................................\n"
 
-print "Loading Stop Words..."
-load_stop_words()
+print "Do you want to update/build inverted index?[y/n]"
+if raw_input() == 'y':
+    print "Loading Stop Words..."
+    load_stop_words()
+    vectorizer = TfidfVectorizer(tokenizer=normalize, stop_words=stop_words)
 
-print "Building inverted sentence index..."
-vectorizer = TfidfVectorizer(tokenizer=normalize, stop_words=stop_words)
-build_sentence_index()
+    print "Building inverted sentence index..."
+    build_sentence_index()
 
-print "Data munching complete! Use WikiQA now!"
+    print "Data munching complete! Use WikiQA now!"
+    print "Writing the inverted index to", sys.argv[1]
+    write_to_file(file_sentences, sys.argv[1])
 
-print "Complete!\n"
+    print "Complete!\n"
+
+    take_commands()
+else:
+    print "Congrats! You just saved 10s in your life.\n"
+    print "Loading Stop Words..."
+    load_stop_words()
+    vectorizer = TfidfVectorizer(tokenizer=normalize, stop_words=stop_words)
+
+    print "Loading inverted index in memory..."
+    file_sentences = load_index_in_memory()
+    if file_sentences == {}:
+        print "error"
+        exit(-1)
+    print "Loaded inverted index in memory!"
 
 take_commands()
